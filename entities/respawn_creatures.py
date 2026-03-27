dd2b6fe18142a5ffd8713569ff63c3acc1d00736
Arthur290795 <arturche_k@mail.ru>
2026-03-27 22:13:00 +0300

    создание класса respawn_creatures для рандомного создания животных

---
+++ entities/respawn_creatures.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+from random import randint, choice
+
+from entities.entity import Entity
+from entities.grass import Grass
+from entities.herbivores.antelope import Antelope
+from entities.herbivores.giraffe import Giraffe
+from entities.herbivores.herbivore import Herbivore
+from entities.herbivores.zebra import Zebra
+from entities.predators.hyena import Hyena
+from entities.predators.leopard import Leopard
+from entities.predators.lion import Lion
+from entities.predators.predator import Predator
+from entities.rock import Rock
+from entities.tree import Tree
+from enums.coordinates import Coordinates
+
+
+class RespawnCreature:
+    PREDATORS = [Lion, Leopard, Hyena]
+    HERBIVORES = [Antelope, Giraffe, Zebra]
+
+    def __init__(self, game_map: "Map", predators_amount: int, herbivores_amount: int):
+        self.game_map = game_map
+        self.predators_amount = predators_amount
+        self.herbivores_amount = herbivores_amount
+        self.grasses_amount = int(herbivores_amount * 2.5)
+        self.rocks_amount = (predators_amount + herbivores_amount) // 2
+        self.trees_amount = (predators_amount + herbivores_amount) // 2
+
+    def respawn_creatures(self):
+        self.respawn(self.predators_amount, Predator)
+        self.respawn(self.herbivores_amount, Herbivore)
+        self.respawn(self.grasses_amount, Grass)
+        self.respawn(self.rocks_amount, Rock)
+        self.respawn(self.trees_amount, Tree)
+
+    def respawn(self, creature_amount: int, entity: type):
+        creature_respawned = 0
+        while creature_respawned < creature_amount:
+            new_coord = self._get_random_coordinates()
+            if self.game_map.is_empty_cell(new_coord):
+                if entity is Herbivore:
+                    new_entity = self._respawn_herbivore(new_coord)
+                elif entity is Predator:
+                    new_entity = self._respawn_predator(new_coord)
+                else:
+                    new_entity = entity(new_coord)
+                self.game_map.set_entity(new_coord, new_entity)
+                creature_respawned += 1
+
+    def _respawn_herbivore(self, new_coord: Coordinates):
+        creature_parameters = self._get_creature_parameters()
+        hp = creature_parameters["hp"]
+        speed = creature_parameters["speed"]
+        return choice(self.HERBIVORES)(new_coord, hp, speed)
+
+    def _respawn_predator(self, new_coord: Coordinates):
+        creature_parameters = self._get_creature_parameters(is_predator=True)
+        hp = creature_parameters["hp"]
+        speed = creature_parameters["speed"]
+        attack_power = creature_parameters["attack_power"]
+        return choice(self.PREDATORS)(new_coord, hp, speed, attack_power)
+
+    def _get_creature_parameters(self, is_predator=False):
+        creature_parameters = {}
+        hp = randint(7, 20)
+        speed = randint(2, 5)
+        creature_parameters["hp"] = hp
+        creature_parameters["speed"] = speed
+        if is_predator:
+            attack_power = randint(2, 4)
+            creature_parameters["attack_power"] = attack_power
+        return creature_parameters
+
+    def _get_random_coordinates(self) -> Coordinates:
+        new_coord_x = randint(0, self.game_map.width - 1)
+        new_coord_y = randint(0, self.game_map.height - 1)
+        return Coordinates(new_coord_x, new_coord_y)
--- map/game_map.py
+++ map/game_map.py
@@ -1,17 +1,10 @@
 from entities.entity import Entity
 from entities.grass import Grass
-from entities.rock import Rock
-from entities.tree import Tree
-from entities.herbivores.antelope import Antelope
-from entities.herbivores.giraffe import Giraffe
-from entities.herbivores.zebra import Zebra
-from entities.predators.lion import Lion
-from entities.predators.hyena import Hyena
-from entities.predators.leopard import Leopard
 from entities.herbivores.herbivore import Herbivore
 from entities.predators.predator import Predator
 from entities.creature import Creature  # для аннотации is_walkable_cell
 from enums.coordinates import Coordinates
+from entities.respawn_creatures import RespawnCreature
 
 
 class Map:
@@ -19,18 +12,15 @@ class Map:
         self.game_map = {}
         self.width = width
         self.height = height
-        
+        self.respawn_creature = RespawnCreature(self, 4, 4)
 
     def setup_default_positions(self):
-        self.set_entity(Coordinates(1, 1), Lion(Coordinates(1, 1), 10, 1, 2))
-        self.set_entity(Coordinates(2, 2), Leopard(Coordinates(2, 2), 8, 11, 2))
-        self.set_entity(Coordinates(3, 3), Hyena(Coordinates(3, 3), 6, 4, 1))
-        self.set_entity(Coordinates(4, 4), Giraffe(Coordinates(4, 4), 10, 2))
-        self.set_entity(Coordinates(5, 5), Zebra(Coordinates(5, 5), 8, 4))
-        self.set_entity(Coordinates(6, 6), Antelope(Coordinates(6, 6), 5, 5))
-        self.set_entity(Coordinates(7, 7), Grass(Coordinates(7, 7)))
-        self.set_entity(Coordinates(8, 8), Rock(Coordinates(8, 8)))
-        self.set_entity(Coordinates(9, 9), Tree(Coordinates(9, 9)))
+        self.respawn_creature.respawn_creatures()
+        # self.respawn_creature.respawn_predators()
+        # self.respawn_creature.respawn_herbivores()
+        # self.respawn_creature.respawn_rocks()
+        # self.respawn_creature.respawn_grasses()
+        # self.respawn_creature.respawn_trees()
 
     def set_entity(self, coordinates: Coordinates, entity: Entity):
         if self.is_valid_coordinates(coordinates):
